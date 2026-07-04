791431e9f3804a04f6101235bc7b1f9e04aeb7df
leonardoacosta <leo@priceless.dev>
2026-07-04 12:59:27 -0500

    feat(registry): add mx to projects.toml -- copen/raycast/cmux resolution

---
+++ platform/raycast-scripts/mx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title mx
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🛰️
+
+# Documentation:
+# @raycast.description MX Mesh
+# @raycast.author leonardoacosta
+# @raycast.authorURL https://raycast.com/leonardoacosta
+
+zed ssh://homelab/home/nyaptor/dev/mx/
