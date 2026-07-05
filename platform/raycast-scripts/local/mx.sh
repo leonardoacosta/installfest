cdc1eb8f40d5952a6916d246531d614b7eb7cc66
leonardoacosta <leo@priceless.dev>
2026-07-05 18:29:03 -0500

    revert: restore raycast-scripts files swept in by previous commit
    platform/raycast-scripts/{mx.sh,local/mx.sh,open-project.sh,local/open-project.sh}
    were pre-existing unrelated WIP (not part of the workspace-profiles change) that got
    accidentally committed alongside 2a9e5dc because they were already staged in the
    working tree. Restoring them to their pre-2a9e5dc state; generate-profiles is untouched.

---
+++ platform/raycast-scripts/local/mx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title mxl
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🛰️
+
+# Documentation:
+# @raycast.description MX Mesh
+# @raycast.author leonardoacosta
+# @raycast.authorURL https://raycast.com/leonardoacosta
+
+zed ~/dev/mx/
